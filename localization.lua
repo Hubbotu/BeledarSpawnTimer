e160611ca654bf5ff309f3596bb79c987481abaf
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-01-22 14:56:44 +0300

    Add files via upload

---
+++ localization.lua
@@ -0,0 +1,19 @@
+-- localization.lua
+L = {}
+
+-- Russian (ZamestoTV)
+L["Spawn Timer"] = "Таймер появления Беледара"
+L["Next spawn in"] = "Следующее появление Беледара в"
+L["Click to send to General chat."] = "Нажмите, чтобы отправить в общий чат."
+L["Hold Alt and click to send to Guild chat."] = "Удерживайте Alt и щелкните, чтобы отправить в чат гильдии."
+L["You are not in a guild!"] = "Вы не состоите в гильдии!"
+L["Could not find the General chat channel."] = "Не удалось найти канал общего чата."
+L["minimapHide"] = "Кнопка на миникарте скрыта."
+L["minimapShow"] = "Отобразить кнопку миникарте."
+L["beledartimer"] = "Используйте команду \"/beledartimer minimap\" для переключения видимости кнопок на миникарте."
+
+-- Add more languages here as needed
+
+
+-- Return the localization table
+return L
